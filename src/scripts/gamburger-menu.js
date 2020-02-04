f20b8b915f4c412791247bad8f657ca7a4be13f5
SDen4 <asw325@yandex.ru>
2020-02-04 16:27:12 +0500

    improve functions of the popup menu - speed and close by tap on free space

--- src/scripts/gamburger-menu.js
+++ src/scripts/gamburger-menu.js
@@ -1,32 +1,41 @@
 const gambBtnOpen = document.getElementById("gamburger-menu-button");
 const popupMenuClose = document.getElementById("popup-menu-button-close");
 const popupMenu = document.getElementById("popup-menu");
-const curRight = Math.ceil( parseInt( window.getComputedStyle(popupMenu).right) );
-let dur = 0;
-let step = 1;
-let int;
+const sreenLock = document.querySelector(".popup-menu-screen-lock");
+const curRight = Math.round( parseInt( window.getComputedStyle(popupMenu).right)/10 )*10;
+let step = 5;
+let interval, duration;
 
 
 gambBtnOpen.addEventListener("click", function(e) {
     e.preventDefault();
-    dur = curRight;
-    int = setInterval(function(){
-        dur += step;
-        popupMenu.style.right = dur + "px";
-        if (dur === 0) {
-            clearInterval(int);
+    duration = curRight;
+    sreenLock.style.display = "block";
+    interval = setInterval(function(){
+        duration += step;
+        popupMenu.style.right = duration + "px";
+        if (duration >= 0) {
+            clearInterval(interval);
         }
     }, 1);
 })
 
 popupMenuClose.addEventListener("click", function(e) {
     e.preventDefault();
-    dur = 0;
-    int = setInterval(function(){
-        dur -= step;
-        popupMenu.style.right = dur + "px";
-        if (dur === curRight) {
-            clearInterval(int);
+    duration = 0;
+    sreenLock.style.display = "none";
+    interval = setInterval(function(){
+        duration -= step;
+        popupMenu.style.right = duration + "px";
+        if (duration <= curRight) {
+            clearInterval(interval);
         }
     }, 1);
 })
+
+sreenLock.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (e.target == sreenLock && curRight !==0 && duration === 0) {
+        popupMenuClose.click();
+    };
+});
